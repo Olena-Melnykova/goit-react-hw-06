6cbbccdac800b31b326f46d0fd778f0bb668bff9
Olena Melnykova <elena88melnykova@gmail.com>
2024-03-26 23:48:36 +0200

    UPDT2

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,16 +2,17 @@ import { useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/contactsSlice';
 import { Button, ContactContainer, ContactName } from './Contact.styled.jsx';
 
-function Contact({ id, name, number }) {
+function Contact({ id, name, number }) { 
   const dispatch = useDispatch();
 
   return (
     <ContactContainer>
-      <ContactName>{name}: {number}</ContactName>
+      <ContactName>{name}: {number}</ContactName> 
       <Button onClick={() => dispatch(deleteContact(id))}>Delete</Button>
     </ContactContainer>
   );
 }
 
 export default Contact;
+
 
